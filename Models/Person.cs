c5e489dbd50c3f88f8dad010710a72504a2d43ff
Jallendess <jallendess@gmail.com>
2024-07-29 11:15:17 -0400

    Primer commit

---
+++ Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiPerson.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public required string Nombre { get; set; }
+        public required int Edad { get; set; }
+    }
+}
